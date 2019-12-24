f6bdf9ab2edd2439c8d9e47388e23de373fd01d8
Jason Kim <kims07231992@gmail.com>
2019-12-23 19:50:12 -0800

    Added AzureBlob related unit tests

---
+++ DotNetSurfer_Backend/tests/DotNetSurfer_Backend.Test/Units/AzureBlobHandler/AzureBlobHandler_DeleteImageFromStorageAsync.cs
@@ -0,0 +1,25 @@
+﻿using DotNetSurfer_Backend.Core.Interfaces.CDNs;
+using Xunit;
+
+namespace DotNetSurfer_Backend.Test.Units.AzureBlobHandler
+{
+    public class AzureBlobHandler_DeleteImageFromStorageAsync
+    {
+        private readonly ICdnHandler _cdnHandler;
+        private readonly string _fileName;
+
+        public AzureBlobHandler_DeleteImageFromStorageAsync()
+        {
+            this._cdnHandler = AzureBlobTestHelper.GetAzureBlobHandler();
+            this._fileName = AzureBlobTestHelper.GetTestFileName();
+        }
+
+        [Fact]
+        public void Delete_Image_ReturnsTrue()
+        {
+            bool isSuccess = this._cdnHandler.DeleteImageFromStorageAsync(this._fileName).Result;
+
+            Assert.True(isSuccess);
+        }
+    }
+}
---
+++ DotNetSurfer_Backend/tests/DotNetSurfer_Backend.Test/Units/AzureBlobHandler/AzureBlobTestHelper.cs
@@ -0,0 +1,38 @@
+﻿using DotNetSurfer_Backend.Core.Interfaces.CDNs;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DotNetSurfer_Backend.Test.Units.AzureBlobHandler
+{
+    public static class AzureBlobTestHelper
+    {
+        private static IConfigurationRoot _configuration;
+        private static ICdnHandler _cdnHandler;
+        private static string _fileName;
+
+        static AzureBlobTestHelper()
+        {
+            string appsettingPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\src\Worker\DotNetSurfer_Backend.API"));
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(appsettingPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string accountName = _configuration["Blob:AccountName"];
+            string accountKey = _configuration["Blob:AccountKey"];
+            _cdnHandler = new DotNetSurfer_Backend.Infrastructure.CDNs.AzureBlobHandler(accountName, accountKey);
+
+            _fileName = "TEST";
+        }
+
+        public static ICdnHandler GetAzureBlobHandler()
+        {
+            return _cdnHandler;
+        }
+
+        public static string GetTestFileName()
+        {
+            return _fileName;
+        }
+    }
+}
