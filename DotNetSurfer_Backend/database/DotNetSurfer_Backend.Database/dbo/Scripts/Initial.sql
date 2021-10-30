e03adb03a2a46741599083696643b55b89a3b6f7
Jason Kim <kims07231992@gmail.com>
2021-10-29 20:14:21 -0700

    Add initial database setup script

---
+++ DotNetSurfer_Backend/database/DotNetSurfer_Backend.Database/dbo/Scripts/Initial.sql
@@ -0,0 +1,33 @@
+﻿-- CREATE DEFAULT PERMISSION TYPES
+INSERT INTO [dbo].[Permissions] ([PermissionId], [PermissionType])
+VALUES
+	(
+		0,
+		'Admin'
+	),
+	(
+		1,
+		'User'
+	);
+GO
+
+-- CREATE DEFAULT STATUS TYPES
+INSERT INTO [dbo].[Permissions] ([PermissionId], [PermissionType])
+VALUES
+	(
+		0,
+		'Requested'
+	),
+	(
+		1,
+		'Ongoing'
+	),
+	(
+		2,
+		'Pending'
+	),
+	(
+		3,
+		'Completed'
+	);
+GO
